29cdf8aca92a94fac98f2c95ee870fe6bd603f09
waruw <metodiuswaruwu@Metodiuss-MacBook-Pro.local>
2022-02-22 00:15:57 +0700

    “programs”

--- resources/views/pages/about/ourprograms.blade.php
+++ resources/views/pages/about/ourprograms.blade.php
@@ -32,10 +32,12 @@
           <div>
     </div>
     <div class="row content mb-4">
+
+    <!-- EDUCATION -->
     
     <div class="col-md-12 p-3 mb-5 background1 text-white">
-      <div class="row content pt-0 mb-0">
-        <div class="col-6 mb-0">
+      <div class="row pt-0 mb-0">
+        <div class="col mb-0">
     
           <div id="edu1" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
@@ -47,12 +49,21 @@
             <div class="carousel-inner" >
               <div class="carousel-item active ">
                 <img  src="{{asset('img/program/edu/1.jpg')}}" widht="auto" height="400" alt="" />
+                  <div class="carousel-caption d-none d-md-block">
+                    <p>Kegiatan Lomba PAUD Kartini Cibodas - Jawa Barat</p>
+                  </div>
               </div>
               <div class="carousel-item">
                 <img  src="{{asset('img/program/edu/2.jpg')}}" widht="auto" height="400" alt="" />
+                <div class="carousel-caption d-none d-md-block">
+                  <p>Pendampingan Belajar Komunitas Cibodas - Jawa Barat</p>
+                </div>
               </div>
                 <div class="carousel-item">
               <img  src="{{asset('img/program/edu/3.jpg')}}" widht="auto" height="400" alt="" />
+              <div class="carousel-caption d-none d-md-block">
+                <p>Pendampingan Belajar Komunitas Mutiara Abadi - Yogyakarta</p>
+              </div>
                 </div>
             </div>
 
@@ -69,8 +80,9 @@
           </div>
         </div>
 
-        <div class="col-6 pt-4 pt-lg-">
+        <div class="col pt-4 pt-lg-">
           <h4 class="progcolor bold pb-3">EDUCATION</h4>
+          <div>
             <p style="text-align:justify"> 
                 Education is the foundation in raising awareness to becoming a trained, 
                 skilled and trustworthyindividual. <br> <br>
@@ -80,103 +92,139 @@
                 pre-schools, assisted-learning classes and conducts various trainings. 
                 During the pandemic, Wadah has also given access to informal education 
                 to individuals through various FORUMS and WEBINARS.
-            <div> 
+            </div>
+            <div class="pt-4"> 
               <a href="{{ url('/news')}}" class="btn btn-light">Read More</a></p>
             </div>
           </div>
-      </div>
-                
-      </div>
-    
-  
-    <div class="col-md-12 p-3 mb-5 background2 ">
-      <div class="row content pt-0 mb-0">
-        <div class="col-lg-6 mb-0">
-    
-                    <div id="kes1" class="carousel slide" data-bs-ride="carousel">
-                      <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#kes1" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#kes1" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#kes1" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                       </div>
-                      <div class="carousel-inner" >
-                        <div class="carousel-item active">
-                          <img  src="{{asset('img/program/heal/1.jpg')}}" widht="auto" height="400" alt="" />
-                        </div>
-                        <div class="carousel-item">
-                          <img  src="{{asset('img/program/heal/2.jpg')}}" widht="auto" height="400" alt="" />
-                        </div>
-                        <div class="carousel-item">
-                          <img  src="{{asset('img/program/heal/3.jpg')}}" widht="auto" height="400" alt="" />
-                        </div>
-  
-                      </div>
-                      <button class="carousel-control-prev" type="button" data-bs-target="#kes1" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                      </button>
-                      <button class="carousel-control-next" type="button" data-bs-target="#kes1" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                      </button>
-                    </div>
-                  </div>
-                  <div class="col-lg-6 pt-4 pt-lg-">
-                      <h4 class="progcolor bold pb-3">HEALTH</h4>
-                      <p style="text-align:justify"> 
-                        Wadah Foundation supports individuals and groups involved in Health Centers 
-                        and Health Service Centers to help promote awareness in healthy and clean 
-                        lifestyle as well as to develop skills related to health programs. <br> <br>
-                        Through this, it is expected that the said individuals and groups would be 
-                        instrumental in helping their families and communities in healthy living. </p>
-                        <div> <a href="{{ url('/news')}}" class="btn btn-light">Read More</a></p>
-                        </div>
-                  </div>
-                  
-      </div>
+      </div>       
     </div>
 
-    <div class="col-md-12 p-3 mb-5 background3">
-      <div class="row content pt-0 mb-0">
-        <div class="col-lg-6 mb-0">
+    <!-- HEALTH -->
+
+    <div class="col-md-12 p-3 mb-5 background2 text-grey">
+      <div class="row pt-0 mb-0">
+        <div class="col mb-0">
     
-                    <div id="pem1" class="carousel slide" data-bs-ride="carousel">
-                      <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#pem1" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#pem1" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#pem1" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                       </div>
-                      <div class="carousel-inner" >
-                        <div class="carousel-item active">
-                          <img  src="{{asset('img/program/eco/1.jpg')}}" widht="auto" height="400" alt="" />
-                        </div>
-                        <div class="carousel-item">
-                          <img  src="{{asset('img/program/eco/2.jpg')}}" widht="auto" height="400" alt="" />
-                        </div>
-                        <div class="carousel-item">
-                          <img  src="{{asset('img/program/eco/3.jpg')}}" widht="auto" height="400" alt="" />
-                        </div>
-  
-                      </div>
-                      <button class="carousel-control-prev" type="button" data-bs-target="#pem1" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                      </button>
-                      <button class="carousel-control-next" type="button" data-bs-target="#pem1" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                      </button>
-                    </div>
+          <div id="edu1" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#kes1" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#kes1" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#kes1" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+
+            <div class="carousel-inner" >
+              <div class="carousel-item active ">
+                <img  src="{{asset('img/program/heal/1.jpg')}}" widht="auto" height="400" alt="" />
+                  <div class="carousel-caption d-none d-md-block">
+                    <p>Kegiatan Sharing Covid Bersama MSF Komunitas Kampung Beting- Jakarta</p>
                   </div>
-                  <div class="col-lg-6 pt-4 pt-lg- text-white">
-                      <h4 class="progcolor bold pb-3">ECONOMIC DEVELOPMENT</h4>
-                      <p style="text-align:justify"> 
-                        Wadah Foundation assists individuals and families to be trained and skilled in 
-                        managing their own potentials in order for them to become strong, empowered and dignified.
-                        <div> <a href="{{ url('/news')}}" class="btn btn-light">Read More</a></p>
-                        </div>
+              </div>
+              <div class="carousel-item">
+                <img  src="{{asset('img/program/heal/2.jpg')}}" widht="auto" height="400" alt="" />
+                <div class="carousel-caption d-none d-md-block">
+                  <p>Kegiatan Pendampingan Ibu Hamil Klinik Bidan Wiwied Komunitas Cibodas - Jawa Barat</p>
+                </div>
+              </div>
+                <div class="carousel-item">
+                <img  src="{{asset('img/program/heal/3.jpg')}}" widht="auto" height="400" alt="" />
+              <div class="carousel-caption d-none d-md-block">
+                <p>Kegiatan Posyandu Komunitas Koa - NTT</p>
+              </div>
+                </div>
+            </div>
+
+              <button class="carousel-control-prev" type="button" data-bs-target="#kes1" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+
+              <button class="carousel-control-next" type="button" data-bs-target="#kes1" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+
+          </div>
+        </div>
+
+        <div class="col pt-4 pt-lg-">
+          <h4 class="progcolor bold pb-3">HEALTH</h4>
+          <div>
+            <p style="text-align:justify"> 
+              <p style="text-align:justify"> 
+                Wadah Foundation supports individuals and groups involved in Health Centers 
+                and Health Service Centers to help promote awareness in healthy and clean 
+                lifestyle as well as to develop skills related to health programs. <br> <br>
+                Through this, it is expected that the said individuals and groups would be 
+                instrumental in helping their families and communities in healthy living.
+            </div>
+            <div class="pt-4"> 
+              <a href="{{ url('/news')}}" class="btn btn-light">Read More</a></p>
+            </div>
+          </div>
+      </div>       
+    </div>
+
+    <!-- ECONOMIC -->
+
+    <div class="col-md-12 p-3 mb-5 background3 text-white">
+      <div class="row pt-0 mb-0">
+        <div class="col mb-0">
+    
+          <div id="edu1" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#pem1" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#pem1" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#pem1" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+
+            <div class="carousel-inner" >
+              <div class="carousel-item active ">
+                <img  src="{{asset('img/program/eco/1.jpg')}}" widht="auto" height="400" alt="" />
+                  <div class="carousel-caption d-none d-md-block">
+                    <p>Kegiatan Menjahit Komunitas Kampung Beting - Jakarta</p>
                   </div>
-      </div>
+              </div>
+              <div class="carousel-item">
+                <img  src="{{asset('img/program/eco/2.jpg')}}" widht="auto" height="400" alt="" />
+                <div class="carousel-caption d-none d-md-block">
+                  <p>Kegiatan Pertanian Komunitas Cibodas - Jawa Barat</p>
+                </div>
+              </div>
+                <div class="carousel-item">
+                  <img  src="{{asset('img/program/eco/3.jpg')}}" widht="auto" height="400" alt="" />
+              <div class="carousel-caption d-none d-md-block">
+                <p>kegiatan Listrik Tenaga Surya Komunitas Pomat - NTT</p>
+              </div>
+                </div>
+            </div>
+
+              <button class="carousel-control-prev" type="button" data-bs-target="#pem1" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+
+              <button class="carousel-control-next" type="button" data-bs-target="#pem1" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+
+          </div>
+        </div>
+
+        <div class="col pt-4 pt-lg-">
+          <h4 class="progcolor bold pb-3">ECONOMIC DEVELOPMENT</h4>
+          <div>
+            <p style="text-align:justify"> 
+              <p style="text-align:justify"> 
+                Wadah Foundation assists individuals and families to be trained and skilled in 
+                managing their own potentials in order for them to become strong, empowered and dignified.
+            </div>
+            <div class="pt-4"> 
+              <a href="{{ url('/news')}}" class="btn btn-light">Read More</a></p>
+            </div>
+          </div>
+      </div>       
     </div>
   
   </div>
