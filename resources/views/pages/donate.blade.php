f665a80343bba449b4a70cec823f19370becfff9
waruw <metodiuswaruwu@Metodiuss-MacBook-Pro.local>
2022-02-22 19:59:27 +0700

    “donate”

--- resources/views/pages/donate.blade.php
+++ resources/views/pages/donate.blade.php
@@ -7,124 +7,108 @@
 <!-- ======= Breadcrumbs ======= -->
 <section id="breadcrumbs" class="breadcrumbs">
   <div class="container">
-
-    <h2>Donation</h2>
-
+  </div>
 </section><!-- End Breadcrumbs -->
 
-<section class="inner-page">
-        <div class="container">
+<section>
+  <div class="container">
+    <div class="row title">
+      <div class="section-title pt-4">
+        <h2>Donation</h2>
+        <h3>Make A Difference</h3>
+        <h3>Through Life-Changing Gifts
+      </div>
+    </div>
 
-<div class="container">
-        <br>
-        <div class="section-title">
-        <br>
-          <h2>Make A Difference</h2>
-          <h3>Through Life-Changing Gifts
-            <br>
-            <br>
+    <div class="row content mb-3">
+
+      <div class="row donatebg">
+        <div class="col-md-4">
+          <h5>US$ 10<br>
+            IDR 140,000
+        </div>
+
+        <div class="col-md-8">
+          <ul type="disc" style="text-align:left">
+            <li><h5>Counters the effect of malnutrition in a child by providing 
+              a month’s supply of milk</li>
         </div>
       </div>
 
-<div class="container">
+      <div class="row donatebg2">
+        <div class="col-md-4">
+          <h5>US$ 100<br>
+            IDR 1,400,000
+        </div>
 
-<div class="row rounded-bottom">
-    <div class="col-md-2"></div>
-		<div class="col-md-2">
-      <h5>US$ 10<br>
-          IDR 140,000
-      </div>
-		<div class="col-md-2"></div>
-		<div class="col-md-6">
-    <ul type="disc" style="text-align:left">
-      <li><h5>Counters the effect of malnutrition in a child by providing 
-        a month’s supply of milk</li>
-    </div>
-</div> <br><br>
-
-<div class="row">
-    <div class="col-md-2"></div>
-		<div class="col-md-2">
-      <h5>US$ 100<br>
-          IDR 1,400,000
-      </div>
-		<div class="col-md-2"></div>
-		<div class="col-md-6">
-    <ul type="disc" style="text-align:left">
-      <li><h5>Pays for the training and capacity building of pre-school tutors</li>
-      <li><h5>Purchases a month’s supply of medicines for a community clinic</li>
-    </div>
-</div> <br><br>
-
-<div class="row">
-    <div class="col-md-2"></div>
-		<div class="col-md-2">
-      <h5>US$ 150<br>
-          IDR 2,100,000
-      </div>
-		<div class="col-md-2"></div>
-		<div class="col-md-6">
-    <ul type="disc" style="text-align:left">
-      <li><h5>Procures educational materials for a Wadah-assisted community</li>
-    </div>
-</div> <br><br>
-
-<div class="row">
-    <div class="col-md-2"></div>
-		<div class="col-md-2">
-      <h5>US$ 400<br>
-          IDR 5,600,000
-      </div>
-		<div class="col-md-2"></div>
-		<div class="col-md-6">
-    <ul type="disc" style="text-align:left">
-      <li><h5>Purchases a personal computer for a community library</li>
-    </div>
-</div> <br><br>
-
-
-<div class="container">
-        <br>
-        <div class="section-title">
-        <br>
-          <h3>Donate Now
-            <br>
-            <br>
+        <div class="col-md-8">
+          <ul type="disc" style="text-align:left">
+            <li><h5>Pays for the training and capacity building of pre-school tutors</li>
+              <li><h5>Purchases a month’s supply of medicines for a community clinic</li>
         </div>
       </div>
 
+      <div class="row donatebg">
+        <div class="col-md-4">
+          <h5>US$ 150<br>
+            IDR 2,100,000
+        </div>
+
+        <div class="col-md-8">
+          <ul type="disc" style="text-align:left">
+            <li><h5>Procures educational materials for a Wadah-assisted community</li>
+        </div>
+      </div>
+
+      <div class="row donatebg2">
+        <div class="col-md-4">
+          <h5>US$ 400<br>
+            IDR 5,600,000
+        </div>
+
+        <div class="col-md-8">
+          <ul type="disc" style="text-align:left">
+            <li><h5>Purchases a personal computer for a community library</li>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="row title">
+      <div class="section-title pt-4">
+        <h3>Donate Now
+      </div>
+    </div>
+
+    <div class="row content mb-3">
+      <div class="row">
       
-<div class="row">
-<div class="col-md-1"></div>
-		<div class="col-md-1"></div>
-		<div class="col-md-3"> 
-      <h5>VIA BANK TRANSFER<br>
-      <h6>YAYASAN WADAH TITIAN HARAPAN<br>
-      <p>BNI Jakarta Pusat<br>
-      <p>IDR Account: 77-7720-1679<br>
-      <p>USD Account: 77-7201-6772<br>
-      <p>Swift Code BNINIDJA</div>
+        <div class="col-md-1"></div>
+        <div class="col-md-1"></div>
+        <div class="col-md-3"> 
+          <h5>VIA BANK TRANSFER<br>
+          <h6>YAYASAN WADAH TITIAN HARAPAN<br>
+          <p>BNI Jakarta Pusat<br>
+          <p>IDR Account: 77-7720-1679<br>
+          <p>USD Account: 77-7201-6772<br>
+          <p>Swift Code BNINIDJA</div>
 
-		<div class="col-md-1"></div>
-		<div class="col-md-1"></div>
-		<div class="col-md-5">      
-      <h5>VIA PAYPAL</h5><br>
-      <h6>You may also show your support to Wadah Foundation and our 
-        communities by donating via PayPal. Just click on the link below:
-        <br><br>
-        <div class="d-flex justify-content-center justify-content-lg-start">
-        <a href="https://www.paypal.com/paypalme/wadahfoundation" class="btn-get-started scrollto">Donate</a>
-</div>
-
-
-
+        <div class="col-md-1"></div>
+        <div class="col-md-1"></div>
+        <div class="col-md-5">      
+          <h5>VIA PAYPAL</h5><br>
+          <h6>You may also show your support to Wadah Foundation and our 
+            communities by donating via PayPal. Just click on the link below:
+            <br><br>
+            <div class="d-flex justify-content-center justify-content-lg-start">
+            <a href="https://www.paypal.com/paypalme/wadahfoundation" class="btn-get-started scrollto">Donate</a> </h6>
+        </div>
+      </div>
+    </div>
 
 
+  </div>
 </section>
-
-</div>
-
-<br><br>
 
 </main><!-- End #main -->
 
