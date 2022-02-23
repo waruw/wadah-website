3ff24cb20490ab48d98394a996ac719cf98ddaed
waruw <metodiuswaruwu@Metodiuss-MacBook-Pro.local>
2022-02-23 16:02:41 +0700

    “newsblade”

--- resources/views/pages/media/news/news.blade.php
+++ resources/views/pages/media/news/news.blade.php
@@ -10,22 +10,24 @@
   </section><!-- garis atas -->
 
 <section>
-  <div class="container">
+    <div class="container">
     <div class="row title">
         <div class="section-title pt-4">
             <h2>Wadah News</h2>
         </div>
     </div>
     <div class="row content">
-      
-        <div class="col-md-8">
-            @foreach ($allNews as $news)
-                <div class="col-lg-12 mb-5">
-                    <div class="position-relative mb-6 border-end-0">
+    
+        <div class="row">
+            <div class="col-md-8">
+                
+                @foreach ($allNews as $news)
+                <div class="col-lg-12 mb-5 newslastpost">
+                    <div class="position-relative">
                         <img class="img-fluid w-100" src="{{asset($news->image)}}" 
                         style="object-fit: cover; min-height:500px; max-height:300px; widht:auto;">
-                        <div class="overlay position-relative bg-light">
-                            <a class="h4 fw-bolder" href="{{route('news.show', $news->slug)}}">{{$news->title}}</a>
+                        <div class="overlay position-relative">
+                            <a class="titlelaspost" href="{{route('news.show', $news->slug)}}">{{$news->title}}</a>
                             <div class=" pt-1 mb-2" style="font-size: 14px;">
                                 <a href="">{{$news->category->nama_kategori}}</a>
                                 <small class="align-self-center fw-light fst-italic">{{$news->created_at->format('d/m/Y')}}</small>
@@ -36,42 +38,115 @@
                     </div>
                 </div>
             @endforeach
-            
-                <div class="row content">
-            
-                    <div class="col-6" style="border:1px solid red">
-                        <h3>test</h3>
+                <div class="row">
+                    <div class="col-6">
+                        @foreach ($allNews as $news)
+                        <div class="col-lg-12 mb-5">
+                            <div class="position-relative mb-6 border-end-0">
+                                <img class="img-fluid w-100" src="{{asset($news->image)}}" 
+                                style="object-fit: cover; min-height:200px; max-height:200px; widht:auto;">
+                                <div class="overlay position-relative bg-light">
+                                    <a class="h4 fw-bolder" href="{{route('news.show', $news->slug)}}">{{$news->title}}</a>
+                                    <div class=" pt-1 mb-2" style="font-size: 14px;">
+                                        <a href="">{{$news->category->nama_kategori}}</a>
+                                        <small class="align-self-center fw-light fst-italic">{{$news->created_at->format('d/m/Y')}}</small>
+                                    </div>
+                                    <p style="text-align:justify"; class="pt-3">{{\Illuminate\Support\Str::limit($news->article, 150, '...')}} 
+                                        <a href='{{route('news.show', $news->slug)}}' class="float-end btn btn-sm btn-outline-dark">Read More</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
                     </div>
 
-                    <div class="col-6" style="border:1px solid red">
-                        <h3>test</h3>
+                    <div class="col-6">
+                        @foreach ($allNews as $news)
+                        <div class="col-lg-12 mb-5">
+                            <div class="position-relative mb-6 border-end-0">
+                                <img class="img-fluid w-100" src="{{asset($news->image)}}" 
+                                style="object-fit: cover; min-height:200px; max-height:200px; widht:auto;">
+                                <div class="overlay position-relative bg-light">
+                                    <a class="h4 fw-bolder" href="{{route('news.show', $news->slug)}}">{{$news->title}}</a>
+                                    <div class=" pt-1 mb-2" style="font-size: 14px;">
+                                        <a href="">{{$news->category->nama_kategori}}</a>
+                                        <small class="align-self-center fw-light fst-italic">{{$news->created_at->format('d/m/Y')}}</small>
+                                    </div>
+                                    <p style="text-align:justify"; class="pt-3">{{\Illuminate\Support\Str::limit($news->article, 150, '...')}} 
+                                        <a href='{{route('news.show', $news->slug)}}' class="float-end btn btn-sm btn-outline-dark">Read More</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
                     </div>
 
+                    <div class="col-6">
+                        @foreach ($allNews as $news)
+                        <div class="col-lg-12 mb-5">
+                            <div class="position-relative mb-6 border-end-0">
+                                <img class="img-fluid w-100" src="{{asset($news->image)}}" 
+                                style="object-fit: cover; min-height:200px; max-height:200px; widht:auto;">
+                                <div class="overlay position-relative bg-light">
+                                    <a class="h4 fw-bolder" href="{{route('news.show', $news->slug)}}">{{$news->title}}</a>
+                                    <div class=" pt-1 mb-2" style="font-size: 14px;">
+                                        <a href="">{{$news->category->nama_kategori}}</a>
+                                        <small class="align-self-center fw-light fst-italic">{{$news->created_at->format('d/m/Y')}}</small>
+                                    </div>
+                                    <p style="text-align:justify"; class="pt-3">{{\Illuminate\Support\Str::limit($news->article, 150, '...')}} 
+                                        <a href='{{route('news.show', $news->slug)}}' class="float-end btn btn-sm btn-outline-dark">Read More</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+
+                    <div class="col-6">
+                        @foreach ($allNews as $news)
+                        <div class="col-lg-12 mb-5">
+                            <div class="position-relative mb-6 border-end-0">
+                                <img class="img-fluid w-100" src="{{asset($news->image)}}" 
+                                style="object-fit: cover; min-height:200px; max-height:200px; widht:auto;">
+                                <div class="overlay position-relative bg-light">
+                                    <a class="h4 fw-bolder" href="{{route('news.show', $news->slug)}}">{{$news->title}}</a>
+                                    <div class=" pt-1 mb-2" style="font-size: 14px;">
+                                        <a href="">{{$news->category->nama_kategori}}</a>
+                                        <small class="align-self-center fw-light fst-italic">{{$news->created_at->format('d/m/Y')}}</small>
+                                    </div>
+                                    <p style="text-align:justify"; class="pt-3">{{\Illuminate\Support\Str::limit($news->article, 150, '...')}} 
+                                        <a href='{{route('news.show', $news->slug)}}' class="float-end btn btn-sm btn-outline-dark">Read More</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
                 </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="bg-light py-2 px-4 mb-3">
-                <h3 class="m-0">Other Wadah News:</h3>
             </div>
-            @foreach ($allReads as $news)
-            <div class="d-flex mb-3">
-                    <img class="img-fluid w-100" src="{{asset($news->image)}}" 
-                        style="object-fit: cover; min-height:100px; max-height:100px; widht:auto;">
-                    <div class="w-100 d-flex flex-column justify-content-center bg-light px-3" style="height: 100px;">
-                                                
-                    <div class="mb-1 capother">
-                        <a href='{{route('news.show', $news->slug)}}'>{{$news->title}}</a>
-                    <div class="mb-1 capcatdate">
-                        <a href="">{{$news->category->nama_kategori}}</a><br>
-                        <span>{{$news->created_at->format('d/m/Y')}}</span>
-                    </div >
-                    </div>
-                </div>
-            </div>  
-            @endforeach  
-        </div>
 
+            <div class="col-md-4">
+                <div class="bg-light py-2 px-4 mb-3">
+                    <h3 class="m-0">Other Wadah News:</h3>
+                </div>
+                @foreach ($allReads as $news)
+                <div class="d-flex mb-3">
+                        <img class="img-fluid w-100" src="{{asset($news->image)}}" 
+                            style="object-fit: cover; min-height:100px; max-height:100px; widht:auto;">
+                        <div class="w-100 d-flex flex-column justify-content-center bg-light px-3" style="height: 100px;">
+                                                    
+                        <div class="mb-1 capother">
+                            <a href='{{route('news.show', $news->slug)}}'>{{$news->title}}</a>
+                        <div class="mb-1 capcatdate">
+                            <a href="">{{$news->category->nama_kategori}}</a><br>
+                            <span>{{$news->created_at->format('d/m/Y')}}</span>
+                        </div >
+                        </div>
+                    </div>
+                </div>  
+                @endforeach  
+            </div>
+
+      </div>
+    </div>
+  </section>
+  
 
 </main><!-- End #main -->
 
