845c5fbe73130cba1032a46acfc03223e4c6d111
waruw <metodiuswaruwu@Metodiuss-MacBook-Pro.local>
2022-04-26 19:52:59 +0700

    founder

--- resources/views/pages/about/aboutthefounder.blade.php
+++ resources/views/pages/about/aboutthefounder.blade.php
@@ -21,30 +21,38 @@
       @if (App::currentLocale()=="en")
         <div class="row content mb-4">
           <p  style="text-align:justify;"> 
-            <img style="float:left; margin-right: 18px; margin-bot: 40px" src="{{asset('img/founder/ahd1.jpg')}}" widht="auto" height="230";>
-              Mrs Anie Harjati Djojohadikusumo 
-              is the founder and President of Wadah Titian Harapan Foundation, an Indonesia -based international 
-              foundation established by women for women and their families.
+            <img style="float:left; margin-right: 18px; margin-bot: 40px" src="{{asset('img/founder/2.jpg')}}" widht="auto" height="230";>
+              Mrs. Anie Harjati Djojohadikusumo is the Founder and currently the President of Wadah Titian Harapan 
+              Foundation (“Wadah”), a Jakarta-based international social organization. She was born in Nganjuk, 
+              East Java, on 9 August 1956. She is, first and foremost, a wife and mother of three children. 
               <br> <br>
-              WADAH from the onset is an acronym derived from Wanita dan Harapan or Women and Hope; Titian Harapan, “a Bridge of Hope”.  
-              As the organization, its scope, its experiences and scale grew, it soon became clear that WADAH was driven by a much greater, 
-              much wider force and more meaningful purpose – that is, the people in the communities that it embrace and serve.  
-              Thus, WADAH now stands for Warga dengan Harapan or “People in the communities with Hope”.
+              WADAH stands by its vision: <i>“Envisages empowered and dignified individuals and communities.”</i> 
+              To achieve its vision, Wadah is guided by its mission of: <i>“Empowering individuals and communities 
+              by utilizing a holistic blend of education, health and economic development programs.”</i> 
               <br> <br>
-              Wadah Foundation grew out of a need to bring hope to people by helping them help themselves and share a better future for their 
-              families and their community.  Wadah was founded in 2008 to specifically address the special needs of women as they participate in social, 
-              educational, community and cultural activities.
+              Operating at the impoverished and underprivileged levels of society, Wadah supports Indonesian women 
+              and their children by offering the opportunity to manage their lives beyond family survival, lifting 
+              them out of the spiral of illiteracy and poverty. 
               <br> <br>
-              WADAH stands by its vision “Empowerment, education, economic self-sufficiency for all.” To achieve aforesaid vision it is guided by its mission, 
-              “To enable impact leaders- men and women – to bring hope, care, change to their communities to achieve millennium development goals.”
+              She was vastly enriched by her proximity to some of the best teachers imaginable, her late parents and 
+              father-in-law, Prof. Dr. Sumitro Djojohadikusumo, who to this day, remains a legend in his country. 
+              By attending seminars and workshops conducted by the Young Presidents’ Organization (YPO), the World 
+              Economic Forum, CNN and countless others, Ibu Anie was inspired to stage her very own Wadah Gatherings. 
               <br> <br>
-              Operating at impoverished and underprivileged levels in the society, WADAH supports Indonesian women and their children by offering the 
-              opportunity to manage their lives beyond family survival, lifting them out of the spiral of illiteracy and poverty. 
-              To date, Wadah has and continuously supports thousands of underprivileged children from communities directly being assisted by Wadah as well 
-              as partner organizations within and outside of Indonesia i.e., the Philippines, Nepal, India and Africa.
+              These Wadah Gatherings serve as a venue for heroes and champions, sung and (mostly) unsung alike to tell 
+              their stories of sharing, caring and giving to inspire each other. Those gatherings were not conferences, 
+              forums, seminars, workshops, but a gathering of like-minded and like-spirited individuals who are already 
+              doing good and aspire to do great. 
               <br> <br>
-              In 2016, through the leadership of Mrs. Djojohadikusumo, Wadah Foundation has been given a Special Consultative Status at the UN Economic 
-              and Social Council (ECOSOC).
+              To date, Wadah has supported thousands of underprivileged children in/through(?) communities directly being 
+              assisted by Wadah, as well as partner organizations within and outside of Indonesia, i.e. the Philippines, 
+              Nepal, India, Bhutan, and Malaysia. 
+              <br> <br>
+              Under Ibu Anie's leadership, Wadah has achieved notable achievements: in 2016, Wadah received a Special 
+              Consultative Status at the United Nations Economic and Social Council (ECOSOC); in 2017, Wadah successfully 
+              obtained ISO 9001-2015 Quality Management System certificate; and in 2018,  Wadah became an Affiliate 
+              Member of the Massachusetts Institute of Technology (MIT) under the Abdul Latif Jameel World Education 
+              Lab or JWEL. 
           </p>
         </div>
 
@@ -52,29 +60,37 @@
       <div class="row content mb-4">
         <p  style="text-align:justify;"> 
           <img style="float:left; margin-right: 18px; margin-bot: 40px" src="{{asset('img/founder/ahd1.jpg')}}" widht="auto" height="230";>
-            Mrs Anie Harjati Djojohadikusumo 
-            adalah the founder and President of Wadah Titian Harapan Foundation, an Indonesia -based international 
-            foundation established by women for women and their families.
-            <br> <br>
-            WADAH from the onset is an acronym derived from Wanita dan Harapan or Women and Hope; Titian Harapan, “a Bridge of Hope”.  
-            As the organization, its scope, its experiences and scale grew, it soon became clear that WADAH was driven by a much greater, 
-            much wider force and more meaningful purpose – that is, the people in the communities that it embrace and serve.  
-            Thus, WADAH now stands for Warga dengan Harapan or “People in the communities with Hope”.
-            <br> <br>
-            Wadah Foundation grew out of a need to bring hope to people by helping them help themselves and share a better future for their 
-            families and their community.  Wadah was founded in 2008 to specifically address the special needs of women as they participate in social, 
-            educational, community and cultural activities.
-            <br> <br>
-            WADAH stands by its vision “Empowerment, education, economic self-sufficiency for all.” To achieve aforesaid vision it is guided by its mission, 
-            “To enable impact leaders- men and women – to bring hope, care, change to their communities to achieve millennium development goals.”
-            <br> <br>
-            Operating at impoverished and underprivileged levels in the society, WADAH supports Indonesian women and their children by offering the 
-            opportunity to manage their lives beyond family survival, lifting them out of the spiral of illiteracy and poverty. 
-            To date, Wadah has and continuously supports thousands of underprivileged children from communities directly being assisted by Wadah as well 
-            as partner organizations within and outside of Indonesia i.e., the Philippines, Nepal, India and Africa.
-            <br> <br>
-            In 2016, through the leadership of Mrs. Djojohadikusumo, Wadah Foundation has been given a Special Consultative Status at the UN Economic 
-            and Social Council (ECOSOC).
+          Mrs. Anie Harjati Djojohadikusumo is the Founder and currently the President of Wadah Titian Harapan 
+          Foundation (“Wadah”), a Jakarta-based international social organization. She was born in Nganjuk, 
+          East Java, on 9 August 1956. She is, first and foremost, a wife and mother of three children. 
+          <br> <br>
+          WADAH stands by its vision: <i>“Envisages empowered and dignified individuals and communities.”</i> 
+          To achieve its vision, Wadah is guided by its mission of: <i>“Empowering individuals and communities 
+          by utilizing a holistic blend of education, health and economic development programs.”</i> 
+          <br> <br>
+          Operating at the impoverished and underprivileged levels of society, Wadah supports Indonesian women 
+          and their children by offering the opportunity to manage their lives beyond family survival, lifting 
+          them out of the spiral of illiteracy and poverty. 
+          <br> <br>
+          She was vastly enriched by her proximity to some of the best teachers imaginable, her late parents and 
+          father-in-law, Prof. Dr. Sumitro Djojohadikusumo, who to this day, remains a legend in his country. 
+          By attending seminars and workshops conducted by the Young Presidents’ Organization (YPO), the World 
+          Economic Forum, CNN and countless others, Ibu Anie was inspired to stage her very own Wadah Gatherings. 
+          <br> <br>
+          These Wadah Gatherings serve as a venue for heroes and champions, sung and (mostly) unsung alike to tell 
+          their stories of sharing, caring and giving to inspire each other. Those gatherings were not conferences, 
+          forums, seminars, workshops, but a gathering of like-minded and like-spirited individuals who are already 
+          doing good and aspire to do great. 
+          <br> <br>
+          To date, Wadah has supported thousands of underprivileged children in/through(?) communities directly being 
+          assisted by Wadah, as well as partner organizations within and outside of Indonesia, i.e. the Philippines, 
+          Nepal, India, Bhutan, and Malaysia. 
+          <br> <br>
+          Under Ibu Anie's leadership, Wadah has achieved notable achievements: in 2016, Wadah received a Special 
+          Consultative Status at the United Nations Economic and Social Council (ECOSOC); in 2017, Wadah successfully 
+          obtained ISO 9001-2015 Quality Management System certificate; and in 2018,  Wadah became an Affiliate 
+          Member of the Massachusetts Institute of Technology (MIT) under the Abdul Latif Jameel World Education 
+          Lab or JWEL.
         </p>
       </div>
       @endif
